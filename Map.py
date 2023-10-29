183aab13bf5ede54668873ba5aa39be5630ff3b3
JeongJinbong <jbong0104@naver.com>
2023-10-29 21:48:09 +0900

    맵 이미지 최적화

--- Map.py
+++ Map.py
@@ -1,8 +1,10 @@
 from pico2d import load_image
 
 class Map:
+    image = None
     def __init__(self, x= 400, y= 300):
-        self.image = load_image('Resource/Image/BackGround.png')
+        if self.image == None:
+            self.image = load_image('Resource/Image/BackGround.png')
         self.x, self.y = x, y
 
     def draw(self):
