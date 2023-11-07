df8015e3f690ba6ca6d6c346b8583fddaaa6f85c
JeongJinbong <jbong0104@naver.com>
2023-11-08 00:18:58 +0900

    상태 머신 추가 (3)

--- Pikachu.py
+++ Pikachu.py
@@ -3,34 +3,38 @@ from pico2d import load_image
 
 class Idle:
     @staticmethod
-    def enter():
-        print('Idle Enter')
+    def enter(pikachu):
+        pikachu.frame = 5
 
     @staticmethod
-    def exit():
-        print('Idle Exit')
-
-    @staticmethod
-    def do():
-        print('Idle Do')
-
-    @staticmethod
-    def draw():
+    def exit(pikachu):
         pass
+
+    @staticmethod
+    def do(pikachu):
+        pikachu.frame = (pikachu.frame - 1)
+        if pikachu.frame < 0:
+            pikachu.frame =5
+
+    @staticmethod
+    def draw(pikachu):
+        pikachu.image.clip_draw(2 + pikachu.frame * 66, 885 - 1 - 332, 66, 66, pikachu.x, pikachu.y, 132, 132)
 
 
 class StateMachine:
-    def __init__(self):
+    def __init__(self, pikachu):
+        self.pikachu = pikachu
         self.cur_state = Idle
 
     def start(self):
-        self.cur_state.enter()
+        self.cur_state.enter(self.pikachu)
 
     def update(self):
-        self.cur_state.do()
+        self.cur_state.do(self.pikachu)
 
     def draw(self):
-        self.cur_state.draw()
+        self.cur_state.draw(self.pikachu)
+
 
 class Pikachu:
     image = None
@@ -40,7 +44,7 @@ class Pikachu:
         self.frame = 0
         if Pikachu.image == None:
             Pikachu.image = load_image('Resource/Image/sprite_sheet.png')
-        self.state_machine = StateMachine()
+        self.state_machine = StateMachine(self)
         self.state_machine.start()
 
     def update(self):
@@ -51,5 +55,3 @@ class Pikachu:
 
     def handle_event(self, event):
         pass
-
-
