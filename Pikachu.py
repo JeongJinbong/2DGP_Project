af44a6fb80691112f223adfc2df3a0e707b639b5
JeongJinbong <jbong0104@naver.com>
2023-11-07 23:02:46 +0900

    상태 머신 추가(2)

--- Pikachu.py
+++ Pikachu.py
@@ -1,6 +1,37 @@
 from pico2d import load_image
 
 
+class Idle:
+    @staticmethod
+    def enter():
+        print('Idle Enter')
+
+    @staticmethod
+    def exit():
+        print('Idle Exit')
+
+    @staticmethod
+    def do():
+        print('Idle Do')
+
+    @staticmethod
+    def draw():
+        pass
+
+
+class StateMachine:
+    def __init__(self):
+        self.cur_state = Idle
+
+    def start(self):
+        self.cur_state.enter()
+
+    def update(self):
+        self.cur_state.do()
+
+    def draw(self):
+        self.cur_state.draw()
+
 class Pikachu:
     image = None
 
@@ -9,43 +40,16 @@ class Pikachu:
         self.frame = 0
         if Pikachu.image == None:
             Pikachu.image = load_image('Resource/Image/sprite_sheet.png')
+        self.state_machine = StateMachine()
+        self.state_machine.start()
 
     def update(self):
-        self.frame = (self.frame + 1) % 5
-        self.x += 5
+        self.state_machine.update()
 
     def draw(self):
-        self.image.clip_draw(2 + self.frame * 66, 885 - 1 - 332, 66, 66, self.x, self.y, 132, 132)
+        self.state_machine.draw()
 
     def handle_event(self, event):
         pass
 
 
-    class Idle:
-        @staticmethod
-        def enter():
-            print('Idle Enter')
-        @staticmethod
-        def exit():
-            print('Idle Exit')
-
-        @staticmethod
-        def do():
-            print('Idle Do')
-
-        @staticmethod
-        def draw():
-            pass
-
-    class StateMachine:
-        def __init__(self):
-            self.cur_state = Idle
-
-        def start(self):
-            self.cur_state.enter()
-
-        def update(self):
-            self.cur_state.do()
-
-        def draw(self):
-            self.cur_state.draw()
